f599465f10837b931e6f852b480bf0cf3240279a
F1729968 Carlos Abreu Custodio <carlos.abreu@bb.com.br>
2020-12-31 12:26:04 -0300

    solution

--- app/src/main/java/br/com/teste/forms/FragmentoBonitinho.kt
+++ app/src/main/java/br/com/teste/forms/FragmentoBonitinho.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 class FragmentoBonitinho: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment, container, false)
-        return rootView
+        return inflater.inflate(R.layout.fragment, container, false)
     }
 }
