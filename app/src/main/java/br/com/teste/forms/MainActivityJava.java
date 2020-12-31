c7e17a92240e4a5cf8487ff56fec9ef970abedd2
F1729968 Carlos Abreu Custodio <carlos.abreu@bb.com.br>
2020-12-31 09:23:49 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/teste/forms/MainActivityJava.java
@@ -0,0 +1,13 @@
+package br.com.teste.forms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_java2);
+    }
+}
